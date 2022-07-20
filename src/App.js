843bd1e0bf802aa99179656db7859ce222ba0df0
thetommedia@gmail.com <thetommedia@gmail.com>
2022-07-20 16:17:09 +0100

    Added

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import PaymentPage from './screens/PaymentPage';
 import ConfirmOrderPage from './screens/ConfirmOrderPage';
 import OrderPage from './screens/OrderPage';
 // import MyAccountPage from './screens/MyAccountPage';
-import MyOrdersPage from './screens/MyOrdersPage';
+// import MyOrdersPage from './screens/MyOrdersPage';
 import ProductsPage from './screens/ProductsPage';
 // import AdminPage from './screens/AdminPage';
 // import AdminOrderListPage from './screens/AdminOrderListPage';
@@ -74,7 +74,7 @@ const App = () => {
                   <Route path="/confirm-order" element={<ConfirmOrderPage />} />
                   <Route path="/order/:id" element={<OrderPage />} />
                   {/* <Route path="/my-account" element={<MyAccountPage />} /> */}
-                  <Route path="/my-orders" element={<MyOrdersPage />} />
+                  {/* <Route path="/my-orders" element={<MyOrdersPage />} /> */}
 
                   {/* <Route path="/admin" element={<AdminPage />} /> */}
                   {/* <Route path="/all-orders" element={<AdminOrderListPage />} /> */}
