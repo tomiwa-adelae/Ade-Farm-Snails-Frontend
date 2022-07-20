b96376480db5c313ba426814783b178b01634b1f
thetommedia@gmail.com <thetommedia@gmail.com>
2022-07-20 13:47:17 +0100

    Added all pages

--- src/App.js
+++ src/App.js
@@ -6,32 +6,32 @@ import store from './store';
 import Homepage from './screens/Homepage';
 import Header from './components/Header';
 import Footer from './components/Footer';
-// import AboutPage from './screens/AboutPage';
-// import ContactPage from './screens/ContactPage';
-// import ProductPage from './screens/ProductPage';
-// import CartPage from './screens/CartPage';
-// import LoginPage from './screens/LoginPage';
-// import RegisterPage from './screens/RegisterPage';
-// import EmailVerify from './screens/EmailVerify';
-// import ShippingPage from './screens/ShippingPage';
-// import ForgotPasswordPage from './screens/ForgotPasswordPage';
-// import PasswordReset from './screens/PasswordReset';
-// import PaymentPage from './screens/PaymentPage';
-// import ConfirmOrderPage from './screens/ConfirmOrderPage';
-// import OrderPage from './screens/OrderPage';
-// import MyAccountPage from './screens/MyAccountPage';
-// import MyOrdersPage from './screens/MyOrdersPage';
-// import ProductsPage from './screens/ProductsPage';
-// import AdminPage from './screens/AdminPage';
-// import AdminOrderListPage from './screens/AdminOrderListPage';
-// import AdminProductListPage from './screens/AdminProductListPage';
-// import AdminUserListPage from './screens/AdminUsersListPage';
-// import UserPage from './screens/UserPage';
+import AboutPage from './screens/AboutPage';
+import ContactPage from './screens/ContactPage';
+import ProductPage from './screens/ProductPage';
+import CartPage from './screens/CartPage';
+import LoginPage from './screens/LoginPage';
+import RegisterPage from './screens/RegisterPage';
+import EmailVerify from './screens/EmailVerify';
+import ShippingPage from './screens/ShippingPage';
+import ForgotPasswordPage from './screens/ForgotPasswordPage';
+import PasswordReset from './screens/PasswordReset';
+import PaymentPage from './screens/PaymentPage';
+import ConfirmOrderPage from './screens/ConfirmOrderPage';
+import OrderPage from './screens/OrderPage';
+import MyAccountPage from './screens/MyAccountPage';
+import MyOrdersPage from './screens/MyOrdersPage';
+import ProductsPage from './screens/ProductsPage';
+import AdminPage from './screens/AdminPage';
+import AdminOrderListPage from './screens/AdminOrderListPage';
+import AdminProductListPage from './screens/AdminProductListPage';
+import AdminUserListPage from './screens/AdminUsersListPage';
+import UserPage from './screens/UserPage';
 import ScrollToTop from './components/ScrollToTop';
-// import AdminOrderPage from './screens/AdminOrderPage';
-// import SearchPage from './screens/SearchPage';
-// import AdminProductPage from './screens/AdminProductPage';
-// import NotFoundPage from './screens/NotFoundPage';
+import AdminOrderPage from './screens/AdminOrderPage';
+import SearchPage from './screens/SearchPage';
+import AdminProductPage from './screens/AdminProductPage';
+import NotFoundPage from './screens/NotFoundPage';
 
 const App = () => {
    return (
@@ -42,7 +42,7 @@ const App = () => {
             <div className="container">
                <Routes>
                   <Route path="/" element={<Homepage />} />
-                  {/* <Route path="/about" element={<AboutPage />} />
+                  <Route path="/about" element={<AboutPage />} />
                   <Route path="/contact" element={<ContactPage />} />
                   <Route path="/product/:id" element={<ProductPage />} />
                   <Route path="/products" element={<ProductsPage />} />
@@ -97,7 +97,7 @@ const App = () => {
                      element={<AdminUserListPage />}
                   />
                   <Route path="/user/:id" element={<UserPage />} />
-                  <Route path="*" element={<NotFoundPage />} /> */}
+                  <Route path="*" element={<NotFoundPage />} />
                </Routes>
             </div>
             <Footer />
