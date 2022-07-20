6af3160ddb453a922bc372b623a665cd12317eb1
thetommedia@gmail.com <thetommedia@gmail.com>
2022-07-20 13:50:13 +0100

    Testing delpoying

--- src/App.js
+++ src/App.js
@@ -8,30 +8,30 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import AboutPage from './screens/AboutPage';
 import ContactPage from './screens/ContactPage';
-import ProductPage from './screens/ProductPage';
-import CartPage from './screens/CartPage';
-import LoginPage from './screens/LoginPage';
-import RegisterPage from './screens/RegisterPage';
-import EmailVerify from './screens/EmailVerify';
-import ShippingPage from './screens/ShippingPage';
-import ForgotPasswordPage from './screens/ForgotPasswordPage';
-import PasswordReset from './screens/PasswordReset';
-import PaymentPage from './screens/PaymentPage';
-import ConfirmOrderPage from './screens/ConfirmOrderPage';
-import OrderPage from './screens/OrderPage';
-import MyAccountPage from './screens/MyAccountPage';
-import MyOrdersPage from './screens/MyOrdersPage';
-import ProductsPage from './screens/ProductsPage';
-import AdminPage from './screens/AdminPage';
-import AdminOrderListPage from './screens/AdminOrderListPage';
-import AdminProductListPage from './screens/AdminProductListPage';
-import AdminUserListPage from './screens/AdminUsersListPage';
-import UserPage from './screens/UserPage';
+// import ProductPage from './screens/ProductPage';
+// import CartPage from './screens/CartPage';
+// import LoginPage from './screens/LoginPage';
+// import RegisterPage from './screens/RegisterPage';
+// import EmailVerify from './screens/EmailVerify';
+// import ShippingPage from './screens/ShippingPage';
+// import ForgotPasswordPage from './screens/ForgotPasswordPage';
+// import PasswordReset from './screens/PasswordReset';
+// import PaymentPage from './screens/PaymentPage';
+// import ConfirmOrderPage from './screens/ConfirmOrderPage';
+// import OrderPage from './screens/OrderPage';
+// import MyAccountPage from './screens/MyAccountPage';
+// import MyOrdersPage from './screens/MyOrdersPage';
+// import ProductsPage from './screens/ProductsPage';
+// import AdminPage from './screens/AdminPage';
+// import AdminOrderListPage from './screens/AdminOrderListPage';
+// import AdminProductListPage from './screens/AdminProductListPage';
+// import AdminUserListPage from './screens/AdminUsersListPage';
+// import UserPage from './screens/UserPage';
 import ScrollToTop from './components/ScrollToTop';
-import AdminOrderPage from './screens/AdminOrderPage';
-import SearchPage from './screens/SearchPage';
-import AdminProductPage from './screens/AdminProductPage';
-import NotFoundPage from './screens/NotFoundPage';
+// import AdminOrderPage from './screens/AdminOrderPage';
+// import SearchPage from './screens/SearchPage';
+// import AdminProductPage from './screens/AdminProductPage';
+// import NotFoundPage from './screens/NotFoundPage';
 
 const App = () => {
    return (
@@ -44,14 +44,14 @@ const App = () => {
                   <Route path="/" element={<Homepage />} />
                   <Route path="/about" element={<AboutPage />} />
                   <Route path="/contact" element={<ContactPage />} />
-                  <Route path="/product/:id" element={<ProductPage />} />
-                  <Route path="/products" element={<ProductsPage />} />
-                  <Route path="/cart" element={<CartPage />} />
-                  <Route path="/cart/:id/:qty" element={<CartPage />} />
-                  <Route path="/login" element={<LoginPage />} />
-                  <Route path="/login/:redirect" element={<LoginPage />} />
-                  <Route path="/register" element={<RegisterPage />} />
-                  <Route path="/search" element={<SearchPage />} />
+                  {/* <Route path="/product/:id" element={<ProductPage />} /> */}
+                  {/* <Route path="/products" element={<ProductsPage />} /> */}
+                  {/* <Route path="/cart" element={<CartPage />} /> */}
+                  {/* <Route path="/cart/:id/:qty" element={<CartPage />} /> */}
+                  {/* <Route path="/login" element={<LoginPage />} /> */}
+                  {/* <Route path="/login/:redirect" element={<LoginPage />} /> */}
+                  {/* <Route path="/register" element={<RegisterPage />} /> */}
+                  {/* <Route path="/search" element={<SearchPage />} />
                   <Route path="/search/:keyword" element={<SearchPage />} />
                   <Route
                      path="/register/:redirect"
@@ -97,7 +97,7 @@ const App = () => {
                      element={<AdminUserListPage />}
                   />
                   <Route path="/user/:id" element={<UserPage />} />
-                  <Route path="*" element={<NotFoundPage />} />
+                  <Route path="*" element={<NotFoundPage />} /> */}
                </Routes>
             </div>
             <Footer />
