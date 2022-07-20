9720930ad7d846bf7d8b2026b1f41a6aa25661de
thetommedia@gmail.com <thetommedia@gmail.com>
2022-07-20 14:23:51 +0100

    Added

--- src/screens/MyAccountPage.js
+++ src/screens/MyAccountPage.js
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import Moment from 'react-moment';
-import Showcase from '../components/Showcase';
-import { logoutUser } from '../actions/userActions';
+import {
+   // Link,
+   useNavigate,
+} from 'react-router-dom';
+// import Moment from 'react-moment';
+// import Showcase from '../components/Showcase';
+// import { logoutUser } from '../actions/userActions';
 import { getMyRecentOrders } from '../actions/orderActions';
-import Loader from '../components/Loader';
-import Message from '../components/Message';
-import EditDetailsModal from '../components/EditDetailsModal';
-import ChangePasswordModal from '../components/ChangePasswordModal';
+// import Loader from '../components/Loader';
+// import Message from '../components/Message';
+// import EditDetailsModal from '../components/EditDetailsModal';
+// import ChangePasswordModal from '../components/ChangePasswordModal';
 import { clearErrors } from '../actions/errorActions';
-import Meta from '../components/Meta';
+// import Meta from '../components/Meta';
 
 const MyAccountPage = () => {
    const dispatch = useDispatch();
    const navigate = useNavigate();
 
-   const [openModal, setOpenModal] = useState(false);
-   const [openPasswordModal, setOpenPasswordModal] = useState(false);
+   // const [openModal, setOpenModal] = useState(false);
+   // const [openPasswordModal, setOpenPasswordModal] = useState(false);
 
    const loginState = useSelector((state) => state.login);
    const { user } = loginState;
 
-   const myRecentOrderState = useSelector((state) => state.myRecentOrders);
-   const { loading, orders } = myRecentOrderState;
+   // const myRecentOrderState = useSelector((state) => state.myRecentOrders);
+   // const { loading, orders } = myRecentOrderState;
 
-   const errorState = useSelector((state) => state.error);
-   const { msg } = errorState;
+   // const errorState = useSelector((state) => state.error);
+   // const { msg } = errorState;
 
    useEffect(() => {
       dispatch(clearErrors());
