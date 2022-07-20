17ae994535b22544a0d81103ed358fa3609e7068
thetommedia@gmail.com <thetommedia@gmail.com>
2022-07-20 17:35:56 +0100

    Initial commit

--- src/screens/MyAccountPage.js
+++ src/screens/MyAccountPage.js
@@ -3,13 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 // import Moment from 'react-moment';
 import Showcase from '../components/Showcase';
-// import { logoutUser } from '../actions/userActions';
-import { getUser } from '../actions/userActions';
+import { logoutUser } from '../actions/userActions';
 import { getMyRecentOrders } from '../actions/orderActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import EditDetailsModal from '../components/EditDetailsModal';
-// import ChangePasswordModal from '../components/ChangePasswordModal';
+import ChangePasswordModal from '../components/ChangePasswordModal';
 import { clearErrors } from '../actions/errorActions';
 import Meta from '../components/Meta';
 
@@ -18,123 +17,166 @@ const MyAccountPage = () => {
    const navigate = useNavigate();
 
    const [openModal, setOpenModal] = useState(false);
+   const [openPasswordModal, setOpenPasswordModal] = useState(false);
 
    const loginState = useSelector((state) => state.login);
-   // const { user } = loginState;
+   const { user } = loginState;
 
-   // console.log(loginState);
-
-   // const myRecentOrderState = useSelector((state) => state.myRecentOrders);
-   // const { loading, orders } = myRecentOrderState;
-
-   // console.log(myRecentOrderState);
-
-   const userDetailsState = useSelector((state) => state.userDetails);
-   const { loading, user, orders } = userDetailsState;
+   const myRecentOrderState = useSelector((state) => state.myRecentOrders);
+   const { loading, orders } = myRecentOrderState;
 
    const errorState = useSelector((state) => state.error);
    const { msg } = errorState;
 
    useEffect(() => {
-      if (!loginState.user) {
+      dispatch(clearErrors());
+      if (!user) {
          return navigate('/login/redirect=/');
       }
 
-      dispatch(clearErrors());
-      dispatch(getUser(loginState.user.id));
       dispatch(getMyRecentOrders());
-   }, [dispatch, loginState, navigate]);
+   }, [navigate, user, dispatch]);
 
    return (
       <div className="myaccountpage">
          <Meta title="Ade Farm Snails | My Account" />
          <Showcase
             img="https://res.cloudinary.com/the-tom-media/image/upload/v1658160593/adefarmsnails/5jxktkqTURBXy8zMWI5OWFkYTkyMzllZTg3Y2M3Zjk2Mzc5M2VhZjZhZC5qcGVnkpUDADzNBkDNA4STBc0EsM0Cdg_byvqvr.jpg"
-            title={user ? `${user.firstName} ${user.lastName}` : ''}
+            title={user && `${user.firstName} ${user.lastName}`}
          />
 
-         <div className="myaccount">
-            <div className="content">
-               <div className="head">
-                  <h3>
-                     {user && user.firstName} {user && user.lastName}
-                  </h3>
-                  <h4>{user && user.email}</h4>
-                  <h5>{user && user.phoneNumber}</h5>
-                  <i
-                     onClick={() => setOpenModal(true)}
-                     className="fas fa-edit"
-                  ></i>
-               </div>
+         {user && (
+            <div className="myaccount">
+               <div className="content">
+                  <div className="head">
+                     <h3>
+                        {user.firstName} {user.lastName}
+                     </h3>
+                     <h4>{user.email}</h4>
+                     <h5>{user.phoneNumber}</h5>
+                     <i
+                        onClick={() => setOpenModal(true)}
+                        className="fas fa-edit"
+                     ></i>
+                  </div>
 
-               {openModal && (
-                  <EditDetailsModal
-                     userObj={user && user}
-                     closeModal={() => {
-                        setOpenModal(false);
-                        dispatch(clearErrors());
-                     }}
-                  />
-               )}
-
-               {loading && <Loader />}
-
-               {msg && <Message msg={msg} variant="error" box />}
-
-               <div className="main">
-                  <h3>Orders</h3>
-
-                  {orders && orders.length === 0 && (
-                     <Message
-                        msg="You have no Orders! Order today"
-                        variant="success"
-                        box
+                  {openModal && (
+                     <EditDetailsModal
+                        userObj={user}
+                        closeModal={() => {
+                           setOpenModal(false);
+                           dispatch(clearErrors());
+                        }}
                      />
                   )}
 
-                  {orders &&
-                     orders.map((order) => (
-                        <Link key={order._id} to={`/order/${order._id}`}>
-                           <div className="item-box">
-                              <div className="img">
-                                 <img
-                                    src={order.orderItems[0].image}
-                                    alt={order.orderItems[0].image}
-                                 />
-                              </div>
-                              <div className="name">
-                                 <h5>{order.orderItems[0].name}</h5>
-                              </div>
-                              <div>
-                                 <h5># {order.totalPrice}</h5>
-                              </div>
-                              {/* <div>
-                                 <h5>
-                                    <Moment format="DD MMM YYYY">
-                                       {order.createdAt}
-                                    </Moment>
-                                 </h5>
-                              </div> */}
-                              <div>
-                                 {order.isPaid ? (
+                  <div className="main">
+                     <h3>Orders</h3>
+
+                     {loading && <Loader />}
+
+                     {msg && <Message msg={msg} variant="error" box />}
+
+                     {orders && orders.length === 0 && (
+                        <Message
+                           msg="You have no Orders! Order today"
+                           variant="success"
+                           box
+                        />
+                     )}
+
+                     {orders &&
+                        orders.map((order) => (
+                           <Link key={order._id} to={`/order/${order._id}`}>
+                              <div className="item-box">
+                                 <div className="img">
+                                    <img
+                                       src={order.orderItems[0].image}
+                                       alt={order.orderItems[0].image}
+                                    />
+                                 </div>
+                                 <div className="name">
+                                    <h5>{order.orderItems[0].name}</h5>
+                                 </div>
+                                 <div>
+                                    <h5># {order.totalPrice}</h5>
+                                 </div>
+                                 {/* <div>
                                     <h5>
-                                       {' '}
-                                       <i className="fas fa-check text-success"></i>{' '}
-                                       Paid
+                                       <Moment format="DD MMM YYYY">
+                                          {order.createdAt}
+                                       </Moment>
                                     </h5>
-                                 ) : (
-                                    <h5>
-                                       <i className="fas fa-exclamation-circle text-danger"></i>{' '}
-                                       Not Paid
-                                    </h5>
-                                 )}
+                                 </div> */}
+                                 <div>
+                                    {order.isPaid ? (
+                                       <h5>
+                                          {' '}
+                                          <i className="fas fa-check text-success"></i>{' '}
+                                          Paid
+                                       </h5>
+                                    ) : (
+                                       <h5>
+                                          <i className="fas fa-exclamation-circle text-danger"></i>{' '}
+                                          Not Paid
+                                       </h5>
+                                    )}
+                                 </div>
+                                 <div>
+                                    {order.isDelivered ? (
+                                       <h5>
+                                          {' '}
+                                          <i className="fas fa-check text-success"></i>{' '}
+                                          Delivered
+                                       </h5>
+                                    ) : (
+                                       <h5>
+                                          {' '}
+                                          <i className="fas fa-exclamation-circle text-danger"></i>{' '}
+                                          Not delivered
+                                       </h5>
+                                    )}
+                                 </div>
                               </div>
-                           </div>
-                        </Link>
-                     ))}
+                           </Link>
+                        ))}
+
+                     <Link to="/my-orders">
+                        <button
+                           disabled={orders && orders.length === 0}
+                           className="btn btn-primary button"
+                        >
+                           See all Orders
+                        </button>
+                     </Link>
+                  </div>
+
+                  <div className="buttons">
+                     <button
+                        className="btn btn-primary button"
+                        onClick={() => setOpenPasswordModal(true)}
+                     >
+                        Change Password
+                     </button>
+                     <button
+                        onClick={() => dispatch(logoutUser())}
+                        className="btn btn-danger button"
+                     >
+                        Logout
+                     </button>
+                  </div>
+
+                  {openPasswordModal && (
+                     <ChangePasswordModal
+                        closeModal={() => {
+                           setOpenPasswordModal(false);
+                           dispatch(clearErrors());
+                        }}
+                     />
+                  )}
                </div>
             </div>
-         </div>
+         )}
       </div>
    );
 };
